2ebc9773bce557c287a11844fd5093f6ecb806a0
DataScienceAbubakar <sabiuabu@gmail.com>
2026-02-06 14:10:11 +0000

    Update: Updated Google Maps location to Kano, Nigeria

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -27,7 +27,7 @@ const Contact: React.FC = () => {
 
                         <div className="map-container" style={{ height: '300px', marginTop: '2rem' }}>
                             <iframe
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126844.06348604473!2d3.3330685!3d6.452654!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8b2ae68280c1%3A0xdc9e87a3da574a9c!2sVictoria%20Island%2C%20Lagos!5e0!3m2!1sen!2sng!4v1704380000000!5m2!1sen!2sng"
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3903.742680965561!2d8.516003174874967!3d11.922987488304246!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x11ac2af457975099%3A0x867e891f1fec2be8!2sWGF9%2B5CW%2C%20Umarawa%20700231%2C%20Kano%2C%20Nigeria!5e0!3m2!1sen!2sgh!4v1770386936427!5m2!1sen!2sgh"
                                 width="100%" height="100%" style={{ border: 0 }} allowFullScreen loading="lazy"
                             ></iframe>
                         </div>
